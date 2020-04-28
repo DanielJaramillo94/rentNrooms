b4e27c448f2e99feec00aff37fab34148aa8cda2
Daniel <daniel.jaramillo7@udea.edu.co>
2020-04-27 22:20:11 -0500

    Basic top home screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,110 +2,132 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+Color mainColor = Color(0xFF006BB1);
+Color mainColorLighter = Color(0xFF42BEBD);
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Rent & Rooms',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          // primarySwatch: Colors.blue, ⚠️⚠️
+          ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 300.0,
+            child: Stack(
+              children: <Widget>[
+                ClipPath(
+                  clipper: WaveClipper(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        colors: [mainColorLighter, mainColor],
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Center(
+                    child: Column(
+                      children: <Widget>[
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Flexible(
+                          flex: 5,
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                Text(
+                                  'Alojamientos con\n reserva flexible',
+                                  style: TextStyle(
+                                      fontSize: 20.0, color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 30),
+                                Container(
+                                  width: 280,
+                                  height: 40,
+                                  child: Material(
+                                    elevation: 5.0,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(30.0)),
+                                    child: TextField(
+                                      controller: TextEditingController(
+                                          text: 'Medellín'),
+                                      style: TextStyle(fontSize: 16.0),
+                                      cursorColor: Colors.black38,
+                                      decoration: InputDecoration(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 16.0, vertical: 6),
+                                          border: InputBorder.none,
+                                          suffixIcon: Material(
+                                            elevation: 2,
+                                            borderRadius: BorderRadius.circular(30.0),
+                                            child: Icon(Icons.search),
+                                          )),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+
+    path.lineTo(0, size.height - 40);
+
+    var point1 = Offset(size.width * 0.5, size.height - 40);
+    var point1Control = Offset(size.width * 0.25, size.height);
+    path.quadraticBezierTo(
+        point1Control.dx, point1Control.dy, point1.dx, point1.dy);
+
+    var point2 = Offset(size.width, size.height - 20);
+    var point2Control = Offset(size.width * 0.75, size.height - 80);
+    path.quadraticBezierTo(
+        point2Control.dx, point2Control.dy, point2.dx, point2.dy);
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
