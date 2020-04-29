4ec0544600f0f6560fa7e253a0b5c83db0c93019
Daniel <daniel.jaramillo7@udea.edu.co>
2020-04-29 01:44:02 -0500

    Added cards to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -132,12 +132,45 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           SliverGrid.count(
             crossAxisCount: 2,
-            children: List.generate(20, (index) {
-              return Container(
-                child: Center(
-                  child: Text(
-                    'Item $index',
-                    style: Theme.of(context).textTheme.headline,
+            children: List.generate(6, (index) {
+              const radius = 8.0;
+              const apartments = [ //⚠️⚠️THIS IS JUST FOR TESTING PURPOSES⚠️⚠️
+                'assets/images/apartment1.jpg',
+                'assets/images/apartment2.jpg',
+                'assets/images/apartment3.jpg',
+                ];
+              return Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(7.0),
+                  child: Stack(
+                    alignment: Alignment.bottomCenter,
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image:
+                                    AssetImage(apartments[index%3]),
+                                fit: BoxFit.cover),
+                            borderRadius: BorderRadius.circular(radius)),
+                      ),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                          color: Color(0xAAFFFFFF),
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(radius),
+                            bottomRight: Radius.circular(radius),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(radius + 3),
+                        child: Text(
+                          'Poblado, Medellín',
+                          style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.w400),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               );
