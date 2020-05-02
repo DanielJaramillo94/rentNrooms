8e608aea566a752033e4c251921dd8dcb8a6fcc1
Jorge Hiler <andreshiler@gmail.com>
2020-05-02 14:52:31 -0500

    Added input field and title

--- lib/date.dart
+++ lib/date.dart
@@ -9,77 +9,194 @@ class Date extends StatefulWidget {
 
 class _DateState extends State<Date> {
   DateTime _dateTime;
+  final String _city = "Medellín";
+
+  DateTime _dateFirst = DateTime.now();
+  DateTime _dateLast = DateTime.now().add(Duration(days: 7));
+  String _days = "7";
+
+  final myController = TextEditingController();
 
   final Color mainColor = Color(0xFF006BB1);
   final Color mainColorMiddle = Color(0xFF2195C6);
   final Color mainColorLighter = Color(0xFF42BEBD);
 
+  void datePicker() async {
+    final List<DateTime> picked = await DateRagePicker.showDatePicker(
+        context: context,
+        initialFirstDate: _dateFirst == null ? DateTime.now() : _dateFirst,
+        initialLastDate: _dateLast == null
+            ? DateTime.now().add(Duration(days: 7))
+            : _dateLast,
+        firstDate: new DateTime(2014),
+        lastDate: new DateTime(2025));
+    if (picked != null && picked.length == 2) {
+      print(picked);
+      setState(() {
+        _dateFirst = picked[0];
+        _dateLast = picked[1];
+        _days = _dateLast.difference(_dateFirst).inDays.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Fecha'),
+      ),
       body: SafeArea(
         child: Container(
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(_dateTime == null
-                          ? 'Nothing has been picked yet'
-                          : _dateTime.toString()),
-                      RaisedButton(
-                        child: Text('Pick a date'),
-                        onPressed: () {
-                          showDatePicker(
-                                  context: context,
-                                  initialDate: _dateTime == null
-                                      ? DateTime.now()
-                                      : _dateTime,
-                                  firstDate: DateTime(2020),
-                                  lastDate: DateTime(2021))
-                              .then((datepick) {
-                            print(datepick);
-                            setState(() {
-                              _dateTime = datepick;
-                            });
-                          });
-                        },
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(_dateTime == null
-                          ? 'Nothing has been picked yet'
-                          : _dateTime.toString()),
-                      RaisedButton(
-                          color: mainColor,
-                          onPressed: () async {
-                            final List<DateTime> picked =
-                                await DateRagePicker.showDatePicker(
-                                    context: context,
-                                    initialFirstDate: DateTime(2014),
-                                    initialLastDate: DateTime(2014),
-                                    firstDate: new DateTime(2014),
-                                    lastDate: new DateTime(2025));
-                            if (picked != null && picked.length == 2) {
-                              print(picked);
-                            }
-                          },
-                          child: new Text("Pick date range"))
-                    ],
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: 20.0, bottom: 20.0, top: 10.0),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                child: Row(children: <Widget>[
+                                  Text("Habitaciones en ",
+                                      style: TextStyle(fontSize: 20.0)),
+                                  Text(
+                                    _city,
+                                    style: TextStyle(fontSize: 27.0),
+                                  ),
+                                ]),
+                              ),
+                              Container(
+                                child: Row(children: <Widget>[
+                                  Text("por ",
+                                      style: TextStyle(fontSize: 20.0)),
+                                  Text(
+                                    _days,
+                                    style: TextStyle(fontSize: 27.0),
+                                  ),
+                                  Text(" días ",
+                                      style: TextStyle(fontSize: 20.0))
+                                ]),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: 20.0,
+                                    right: 32.0,
+                                    top: 8.0,
+                                    bottom: 8.0),
+                                child: Text(
+                                    _dateTime == null
+                                        ? 'Fecha de entrada:'
+                                        : _dateTime.toString(),
+                                    style: TextStyle(fontSize: 17.0)),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                    left: 20.0, right: 32.0, top: 8.0),
+                                child: new Row(
+                                  children: <Widget>[
+                                    Container(
+                                      child: new Flexible(
+                                          child: new TextField(
+                                        readOnly: true,
+                                        onTap: datePicker,
+                                        decoration: InputDecoration(
+                                            prefixIcon: Icon(Icons.date_range),
+                                            hintText:
+                                                "${_dateFirst.day.toString()}/${_dateFirst.month.toString()}/${_dateFirst.year.toString()}",
+                                            contentPadding: EdgeInsets.fromLTRB(
+                                                10.0, 10.0, 10.0, 10.0),
+                                            border: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20.0))),
+                                        maxLines: 1,
+                                      )),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: 20.0,
+                                    right: 32.0,
+                                    top: 25.0,
+                                    bottom: 8.0),
+                                child: Text(
+                                    _dateTime == null
+                                        ? 'Fecha de salida:'
+                                        : _dateTime.toString(),
+                                    style: TextStyle(fontSize: 17.0)),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(
+                                    left: 20.0, right: 32.0, top: 8.0),
+                                child: new Row(
+                                  children: <Widget>[
+                                    Container(
+                                      child: new Flexible(
+                                          child: new TextField(
+                                        readOnly: true,
+                                        enableInteractiveSelection: false,
+                                        onTap: datePicker,
+                                        decoration: InputDecoration(
+                                            prefixIcon: Icon(Icons.date_range),
+                                            hintText:
+                                                "${_dateLast.day.toString()}/${_dateLast.month.toString()}/${_dateLast.year.toString()}",
+                                            contentPadding: EdgeInsets.fromLTRB(
+                                                10.0, 10.0, 10.0, 10.0),
+                                            border: OutlineInputBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(
+                                                        20.0))),
+                                        maxLines: 1,
+                                      )),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(bottom: 10.0),
+                          child: RaisedButton.icon(
+                            color: Colors.white,
+                            icon: Icon(Icons.search),
+                            onPressed: () {
+                              print("Buscando...");
+                            },
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(color: mainColorMiddle),
+                              borderRadius: new BorderRadius.circular(18.0),
+                            ),
+                            label: Text(
+                              "Buscar",
+                              style: TextStyle(fontSize: 17.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Container(
                   child: Transform.rotate(
                     angle: pi,
                     child: Container(
-                      height: 110.0,
+                      height: 170.0,
                       child: Stack(
                         children: <Widget>[
                           ClipPath(
