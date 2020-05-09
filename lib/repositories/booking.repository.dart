ec91d2467e3638521b2a2747512ae07f4d407d6b
Jorge Hiler <andreshiler@gmail.com>
2020-05-09 02:26:32 -0500

    Updated consumer HTTP request

--- lib/repositories/booking.repository.dart
+++ lib/repositories/booking.repository.dart
@@ -3,24 +3,37 @@ import 'package:rent_n_rooms/models/booking.model.dart';
 import 'package:rent_n_rooms/models/date_picker.model.dart';
 import 'dart:convert';
 
+import 'package:rent_n_rooms/services/service.dart';
+
 class BookingRepository {
-  final String url =
-      'https://a0f8d7d5-cce0-410a-8fcc-50d521534131.mock.pstmn.io/booking';
+  static Api api = Api();
+  static String url = api.bookingEndPoint('Arrendamientos njs');
 
   Future<DataBooking> createBooking(
       DataBooking booking, DatePicker date) async {
     print("createBookingHttp");
+    print(
+        'Email: ${booking.getEmail()} Id: ${booking.getIdRoom()} Name: ${booking.getName()}');
     try {
       String checkin = date.getDateCheckin().toString().split(" ")[0];
       String checkout = date.getDateCheckout().toString().split(" ")[0];
-      http.Response response = await http.post(url, body: {
-        'checkin': checkin,
-        'checkout': checkout,
-        'email': booking.getEmail(),
-        'name': booking.getName(),
-        'id_room': booking.getIdRoom()
-      });
+      print('checkin $checkin checkout $checkout');
+      print(url);
+
+      http.Response response = await http.post(url,
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(<String, String>{
+            'checkin': '2020-05-09"',
+            'checkout': '2020-05-10',
+            'email': 'pedro@correo.com',
+            'name': 'Pedro',
+            'id_room': '5eb24077bf3587508244c267'
+          }));
       Map data = json.decode(response.body);
+      print("data");
+      print(data);
       DataBooking newBooking = DataBooking(
           data['email'], data['name'], data['id_room'], data['id_booking']);
       return newBooking;
