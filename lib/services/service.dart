89eb964f4e9d6cac6b6bf1ee50755b1abb832c44
linamariaum <lina8452@gmail.com>
2020-05-09 01:15:15 -0500

    Add api Scala in api service

--- lib/services/service.dart
+++ lib/services/service.dart
@@ -1,14 +1,15 @@
 class Api {
-  final String api1 = 'http://ec2-34-195-214-219.compute-1.amazonaws.com:8000';
-  final String api2 = 'https://0kaup1m6dg.execute-api.us-east-1.amazonaws.com/dev';
-  final String api3 = 'http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api';
+  final String api1Python = 'http://ec2-34-195-214-219.compute-1.amazonaws.com:8000';
+  final String api2Lambda = 'https://0kaup1m6dg.execute-api.us-east-1.amazonaws.com/dev';
+  final String api3NodeJs = 'http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api';
+  final String api4Scala = 'http://ec2-18-188-220-151.us-east-2.compute.amazonaws.com/';
 
   List<String> searchEndPoint(String codeCity, DateTime checkin, DateTime checkout) {
     String search =  '/rooms/search?location=$codeCity&checkin=$checkin&checkout=$checkout';
-    String url1 = this.api1+search;
-    String url2 = this.api2+search;
-    String url3 = this.api3+search;
-    List<String> urls = [url1, url2, url3]
+    String url1 = this.api1Python+search;
+    String url2 = this.api2Lambda+search;
+    String url3 = this.api3NodeJs+search;
+    List<String> urls = [url1, url2, url3];
     return urls;
   }
 
@@ -17,13 +18,13 @@ class Api {
     String api = '';
     switch (agencyName) {
       case 'Arrendamientos njs':
-        api = this.api3;
+        api = this.api3NodeJs;
         break;
       case 'S_TEAM':
-        api = this.api1;
+        api = this.api1Python;
         break;
       case 'Lambda Team':
-        api = this.api2;
+        api = this.api2Lambda;
         break;
       default:
         api = '';
@@ -36,14 +37,14 @@ class Api {
     String api = '';
     switch (agencyName) {
       case 'Arrendamientos njs':
-        api = this.api3;
+        api = this.api3NodeJs;
         break;
       case 'S_TEAM':
         booking = '/rooms/booking';
-        api = this.api1;
+        api = this.api1Python;
         break;
       case 'Lambda Team':
-        api = this.api2;
+        api = this.api2Lambda;
         break;
       default:
         api = '';
