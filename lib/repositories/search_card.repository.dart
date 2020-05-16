3365fb0638c6b97498c524dbb9b06cc181848c29
Jorge Hiler <andreshiler@gmail.com>
2020-05-16 00:43:03 -0500

    Listing from various endpoints

--- lib/repositories/search_card.repository.dart
+++ lib/repositories/search_card.repository.dart
@@ -7,72 +7,59 @@ import 'package:rent_n_rooms/providers/date_picker.provider.dart';
 import 'package:rent_n_rooms/services/api.service.dart';
 
 class SearchCardRepository {
-
   Future<int> getLength(DateProvider date, CityProvider codeCity) async {
     String checkin = date.getDates().getDateCheckin().toString().split(" ")[0];
-    String checkout = date.getDates().getDateCheckout().toString().split(" ")[0];
+    String checkout =
+        date.getDates().getDateCheckout().toString().split(" ")[0];
     String code = codeCity.citySelected.codeCity;
-
-    String url = ApiService.searchEndPoint(code, checkin, checkout);
-      http.Response response = await http.get(url);
-      List data = json.decode(response.body);
-      List searchCards = data;
-
-    /*   ***************
-    List<String> urls = ApiService.searchEndPoint(code, checkin, checkout);
-    List<List> searchCards;
-    for (var url in urls) {
-      http.Response response = await http.get(url);
-      List data = json.decode(response.body);
-      searchCards.add(data);
-    }
-      ***************  */
-
-    return searchCards.length;
-  }
-
-  Future<List<SearchCard>> fetchSearchCards(DateProvider date, CityProvider codeCity) async {
-    String checkin = date.getDates().getDateCheckin().toString().split(" ")[0];
-    String checkout = date.getDates().getDateCheckout().toString().split(" ")[0];
-    String code = codeCity.citySelected.codeCity;
-    print('Fechas $checkin $checkout codigo $code');
 
     String url = ApiService.searchEndPoint(code, checkin, checkout);
     http.Response response = await http.get(url);
     List data = json.decode(response.body);
     List searchCards = data;
+    return searchCards.length;
+  }
+
+  Future<List<SearchCard>> fetchSearchCards(
+      DateProvider date, CityProvider codeCity) async {
+    String checkin = date.getDates().getDateCheckin().toString().split(" ")[0];
+    String checkout =
+        date.getDates().getDateCheckout().toString().split(" ")[0];
+    String code = codeCity.citySelected.codeCity;
+    print('Fechas $checkin $checkout codigo $code');
+
+    String url = ApiService.searchEndPoint(code, checkin, checkout);
+
+    http.Response response = await http.get(url);
+    List data = json.decode(response.body);
+    List searchCards = data;
+    //searchCards = await searchAllEndPoint(code, checkin, checkout); //Todos los endpoints
     return searchCards.map((obj) {
       String imgURL = obj['thumbnail'];
       String location = obj['location']['name'];
       String rating = obj['rating'].toString();
       String property = obj['property_name'];
-      num price = obj['price'];
-      String idRoom = obj['id'];
+      num price = obj['price']; 
+      String idRoom = obj['id'].toString();
       String agency = obj['agency']['name'];
-      return SearchCard(imgURL, location, rating, property, price, idRoom, agency);
+      return SearchCard(
+          imgURL, location, rating, property, price, idRoom, agency);
     }).toList();
+  }
 
-    /*  ***************
-    List<String> urls = ApiService.searchEndPoint(code, checkin, checkout);
-    List<SearchCard> cards;
-    for (var url in urls) {
-      http.Response response = await http.get(url);
-      List data = json.decode(response.body);
-      List searchCards = data;
-      return searchCards.map((obj) {
-        String imgURL = obj['thumbnail'];
-        String location = obj['location']['name'];
-        String rating = obj['rating'].toString();
-        String property = obj['property_name'];
-        num price = obj['price'];
-        String idRoom = obj['id'];
-        String agency = obj['agency']['name'];
-        //cards.add(SearchCard(imgURL, location, rating, property, price, idRoom, agency));
-        return SearchCard(imgURL, location, rating, property, price, idRoom, agency);
-      }).toList();
-    }
-    return cards;
-    *******************************   */
-
+  searchAllEndPoint(code, checkin, checkout) async {
+    List urls = ApiService.searchAllEndPoint(code, checkin, checkout);
+    var futures = <Future>[];
+    urls.forEach((url) {
+      print(url);
+      futures.add(http.get(url));
+    });
+    List result = await Future.wait(futures);
+    print(json.decode(result[0].body));
+    List searchCards = [];
+    result.forEach((rooms) {
+      searchCards.addAll(json.decode(rooms.body));
+    });
+    return searchCards;
   }
 }
--- lib/services/api.service.dart
+++ lib/services/api.service.dart
@@ -1,12 +1,28 @@
 class ApiService {
   static String api1Python = 'http://ec2-34-195-214-219.compute-1.amazonaws.com:8000';
-  static String api2Lambda = 'https://0kaup1m6dg.execute-api.us-east-1.amazonaws.com/dev';
+  static String api2Lambda = 'https://0kaup1m6dg.execute-api.us-east-1.amazonaws.com/prod';
   static String api3NodeJs = 'http://ec2-13-58-217-208.us-east-2.compute.amazonaws.com/api';
   static String api4Scala = 'http://ec2-18-188-220-151.us-east-2.compute.amazonaws.com/';
 
   static String searchEndPoint(String codeCity, String checkin, String checkout) {
     String search =  '/rooms/search?location=$codeCity&checkin=$checkin&checkout=$checkout';
-    return api1Python+search;
+    return api2Lambda+search;
+    /* *******************************
+    String url1 = api1Python+search;
+    //String url2 = api2Lambda+search;
+    String url3 = api3NodeJs+search;
+    List<String> urls = [url1, url3];
+    return urls;
+    *******************************  */
+  }
+
+    static List searchAllEndPoint(String codeCity, String checkin, String checkout) {
+      String search =  '/rooms/search?location=$codeCity&checkin=$checkin&checkout=$checkout';
+      List urls = [];
+      urls.add(api2Lambda + search);
+      urls.add(api2Lambda + search);
+      urls.add(api2Lambda + search);
+    return urls;
     /* *******************************
     String url1 = api1Python+search;
     //String url2 = api2Lambda+search;
