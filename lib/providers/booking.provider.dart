b36c87053942c06bd1022cbe09a5990831d42ae3
Jorge Hiler <andreshiler@gmail.com>
2020-05-26 01:38:49 -0500

    Updated booking view
    - Show reservation code and disable 'reservar' button.
    - Update error message with real agency name.
    - Modify booking provider

--- lib/providers/booking.provider.dart
+++ lib/providers/booking.provider.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
 import 'package:rent_n_rooms/models/booking.model.dart';
 import 'package:rent_n_rooms/models/date_picker.model.dart';
 import 'package:rent_n_rooms/repositories/booking.repository.dart';
 
 class BookingProvider with ChangeNotifier {
-  DataBooking _booking = DataBooking('email', 'name', 'idRoom', 'idBooking');
+  DataBooking _booking = DataBooking('email', 'name', 'idRoom', '');
   BookingRepository _bookingRepository = BookingRepository();
 
   void updateBooking(
@@ -13,11 +15,24 @@ class BookingProvider with ChangeNotifier {
     _booking.setEmail(email);
     _booking.setIdRoom(idRoom);
     _booking.setIdBooking(idBooking);
+  }
+
+  void notify(){
     notifyListeners();
+  }
+
+  void updateWithoutNotify(String name, String email,  String idRoom) {
+    _booking.setName(name);
+    _booking.setEmail(email);
+    _booking.setIdRoom(idRoom);
   }
 
   DataBooking getBooking() {
     return _booking;
+  }
+
+  void resetBookingId(){
+    this._booking.setIdBooking('');
   }
 
   Future<DataBooking> createBooking(DatePicker date, String agencia) async {
